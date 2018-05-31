3e8ec2fe0c12ee3a938266c8ab6db7023839d0cf
Tommy Hodgins <tomhodgins@gmail.com>
2018-05-30 20:42:49 -0400

    Update favicon link

--- src/site-info.js
+++ src/site-info.js
@@ -2,6 +2,6 @@ module.exports = {
   title: `BengalCat.Party`,
   description: `Pictures of George Franklin, the adventurous bengal cat`,
   keywords: `bengal cat kitten bengals feline bengal tabby cute adorable animal pictures photo gallery`,
-  favicon: 'src/img/favicon.png',
+  favicon: '/src/img/favicon.png',
   stylesheet: '/style.css'
 }
